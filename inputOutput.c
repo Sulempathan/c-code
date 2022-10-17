b2b91197018bd16298039322074b04ded88bf835
Zeeshan <zeeshan0703@outlook.com>
2022-10-17 11:09:21 +0530

    Updated Syntax.c and Added inputOutput.changes Updated syntax.c file as there was a naming error on the programme so that now is working properly also added a basic file which shows how to use userinput and print the same in C programme

---
+++ inputOutput.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int input;
+    // This programme include input output in C programme to understand how the make a easy start 
+    
+    printf("This is a C programme\n");// this is a print statement which is used to print a line in C programme
+
+    // How to take user input
+    printf("Enter a number:\t");
+    scanf("%d",&input);//this is the syntax to take userinput
+
+    printf("You have entered %d number\n",input);//Printing the userinput
+
+    return 0;
+}
